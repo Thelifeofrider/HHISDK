2f1b649403efa30ef04209f107c51d2b985d6d54
Thelifeofrider <poonkathirvelan.vite@gmail.com>
2022-03-04 18:03:19 +0530

    return

--- Sources/HHISDK/HHISDK.swift
+++ Sources/HHISDK/HHISDK.swift
@@ -1,7 +1,9 @@
 public struct HHISDK {
     public private(set) var text = "Hello, World!"
-    public func updateFirmware() {
+    public func updateFirmware() -> String{
         print("Firmware Updated")
+        
+        return "Firware update initated"
     }
     public init() {
     }
