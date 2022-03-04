39c7d555f6c055446cc2b602bdd1f5ced50ff775
Poonkathirvelan boopalan <poonkathirvelan.vite@gmail.com>
2022-03-04 17:04:24 +0530

    NEW SPM

--- Sources/HHISDK/HHISDK.swift
+++ Sources/HHISDK/HHISDK.swift
@@ -1,6 +1,8 @@
 public struct HHISDK {
     public private(set) var text = "Hello, World!"
-
+    public func updateFirmware() {
+        print("Firmware Updated")
+    }
     public init() {
     }
 }
